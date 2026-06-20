b9c494991efade1cd8f63648604aebb14dd2b588
AryanTitoria <aryantitoria90@gmail.com>
2026-06-20 22:39:58 +0530

    get queue api

--- backend/src/routes/queueRoutes.js
+++ backend/src/routes/queueRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { joinQueue } from "../controllers/queueController.js";
+import { joinQueue, getQueue } from "../controllers/queueController.js";
 
 const router = express.Router();
 
 router.post("/join", joinQueue);
+router.get("/:shopId", getQueue);
 
 export default router;
