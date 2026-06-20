147a563a9d38578e08da625671bbe674789dcd0a
AryanTitoria <aryantitoria90@gmail.com>
2026-06-20 23:26:00 +0530

    improve queue

--- backend/src/routes/queueRoutes.js
+++ backend/src/routes/queueRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { joinQueue, getQueue } from "../controllers/queueController.js";
+import { joinQueue, getQueue, fillChairs } from "../controllers/queueController.js";
 
 const router = express.Router();
 
 router.post("/join", joinQueue);
 router.get("/:shopId", getQueue);
+router.post("/fill-chairs", fillChairs);
 
 export default router;
